58c9f0fadaa5e8704f54b89f1151d17e1b9584d9
CoderTangYin <flashtangyin@sina.com>
2022-07-22 13:45:17 +0800

    ”ss“

---
+++ Algorithm/src/LeetCode/MyPow.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class MyPow {
+    public double myPow(double x, int n) {
+        // 任何数的0次幂都是1
+        if (n == 0) return 1;
+        // 任何数的-1次幂都是它的分之一
+        if (n == -1) return 1 / x;
+        // 判断是奇数
+        boolean odd = (n & 1) == 1;
+        // 使用递归份治 一半
+        double half = myPow(x,n>>1);
+        half *= half;
+        return odd ? (half * x) : half;
+
+    }
+}
