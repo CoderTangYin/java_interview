4c2fd3d2e572ae6d904ee1a353b69729583133c5
CoderTangYin <flashtangyin@sina.com>
2022-07-22 13:24:52 +0800

    ss

--- Algorithm/src/LeetCode/ThreeSum.java
+++ Algorithm/src/LeetCode/ThreeSum.java
@@ -24,13 +24,37 @@ import java.util.List;
  */
 public class ThreeSum {
 
-//    public List<List<Integer>> threeSum(int[] nums) {
-//        if (nums == null) return null;
-//        List<List<Integer>> list = new ArrayList<>();
-//        if (nums.length < 3) return list;
-//        Arrays.sort(nums);
-//
-//    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null) return null;
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums.length < 3) return list;
+        Arrays.sort(nums);
+
+        // 用来扫描的只能扫描到倒是第三个
+        int lastIdx = nums.length - 3;
+        // 最右侧的指针指向数组最后一个元素
+        int lastR = nums.length - 1;
+        for (int i=0; i<lastIdx; i++) {
+            // 自己个自己前边的一样就跳过
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int l = i + 1, r = lastR, remain = -nums[i];
+            while (i < r) {
+                int sumLr = nums[l] + nums[r];
+                if (sumLr == remain) {
+                    list.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                } else if (sumLr < remain) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return list;
+    }
 //    public List<List<Integer>> threeSum(int[] nums) {
 //
 //    }
