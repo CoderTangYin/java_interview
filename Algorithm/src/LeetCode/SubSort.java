ed7cae4deacd98e58f8220d1e1eca7f97a7a661e
CoderTangYin <flashtangyin@sina.com>
2022-07-25 09:42:54 +0800

    ss

--- Algorithm/src/LeetCode/SubSort.java
+++ Algorithm/src/LeetCode/SubSort.java
@@ -10,9 +10,10 @@ public class SubSort {
 	 */
 	public int[] subSort(int[] nums) {
 
+		// 找逆序对 从左到右找到逆序最远的那个索引
 		int max = nums[0];
 		int r = -1;
-		for (int i = 0; i < nums.length; i++) {
+		for (int i = 1; i < nums.length; i++) {
 			int v = nums[i];
 			if (v >= max) {
 				max = v;
