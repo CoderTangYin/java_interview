e2b8ea1118846a303f5a88a8da148c73497a488c
George <>
2023-03-14 20:05:34 +0800

    no message

--- Algorithm/src/LeetCode/DP/MinDistance.java
+++ Algorithm/src/LeetCode/DP/MinDistance.java
@@ -26,7 +26,31 @@ package LeetCode.DP;
 public class MinDistance {
 
     public int minDistance(String word1, String word2) {
-        return 0;
+        if(word1 == null || word2 == null) return 0;
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+        int[][] dp = new int[chars1.length + 1][chars2.length + 1];
+        // 空串转为空串的情况 不需要转换
+        dp[0][0] = 0;
+        // 0列  转换成空串的最少操作数
+        for (int i = 1; i <= chars1.length; i++) {
+            dp[i][0] = i;
+        }
+        // 0行
+        for (int i = 1; i <= chars2.length; i++) {
+            dp[0][i] = 0;
+        }
+        // 其他行跟列
+        for (int i = 1; i <= chars1.length; i++) {
+            for (int j=1; j<=chars2.length; j++) {
+                int top = dp[i-1][j] + 1;
+                int left = dp[i][j-1] + 1;
+                int leftTop = dp[i-1][j-1];
+                if(chars1[i-1] != chars2[j-1]) { leftTop++ };
+                dp[i][j] = Math.min(Math.min(top, left), leftTop);
+            }
+        }
+        return dp[chars1.length][chars2.length];
     }
 
 }
